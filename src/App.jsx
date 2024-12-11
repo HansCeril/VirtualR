736032cbc184d2f390c5a415d583d9641f490306
hans <anselmeceril@gmail.com>
2024-12-11 19:02:46 +0000

    add herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Navbar />
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <HeroSection />
+
+      </div>
+        
+      
+    </>
   )
 }
 
